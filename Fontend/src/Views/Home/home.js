ccd0def111224ae246be87312c9f8425a4a72a43
Fang <thesunskys@gmail.com>
2020-03-15 00:35:38 +0700

    add new component slide

--- Fontend/src/Views/Home/home.js
+++ Fontend/src/Views/Home/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { Row, Col, Button, Input, Card,Upload, Icon, message,notification } from 'antd'
 import { Container } from 'reactstrap';
+import Slider from '../../Components/Slider/Slider'
 import { getPhotoDB } from '../../Services/API'
 import '../../App.css'
 
@@ -38,6 +39,12 @@ export default class home extends Component {
     //     }
     // }
     render(){
+        const images = [
+            'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+            'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
+            'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
+            'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80',
+          ]
         return(
             <div>
                 <Card style={{padding:'50px 50px', backgroundColor: 'rgba(150, 70, 255, 255)', width: 'auto', height: '550px'}}>
@@ -53,10 +60,10 @@ export default class home extends Component {
                                 <h2>Database</h2>
                                 <Row>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoDB[0])}/>
+                                        <img className='imgDB' src={ require('../../img/databases/' + this.state.photoDB[0])}/>
                                     </Col>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoDB[1])}/>
+                                        <img className='imgDB' src={ require('../../img/databases/' + this.state.photoDB[1])}/>
                                     </Col>
                                 </Row>
                             </Col>
@@ -64,26 +71,26 @@ export default class home extends Component {
                                 <h2>Language</h2>
                                 <Row>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[0])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[0])}/>
                                     </Col>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[1])}/>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[2])}/>
-                                    </Col>
-                                    <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[3])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[1])}/>
                                     </Col>
                                 </Row>
                                 <Row>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[4])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[2])}/>
                                     </Col>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[5])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[3])}/>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col className='photoDB' span={9}>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[4])}/>
+                                    </Col>
+                                    <Col className='photoDB' span={9}>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[5])}/>
                                     </Col>
                                 </Row>
                             </Col>
@@ -91,18 +98,18 @@ export default class home extends Component {
                                 <h2>Tool</h2>
                                 <Row>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[5])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[5])}/>
                                     </Col>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[5])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[5])}/>
                                     </Col>
                                 </Row>
                                 <Row>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[5])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[5])}/>
                                     </Col>
                                     <Col className='photoDB' span={9}>
-                                        <img className='imgDB' src={ require('../../img/' + this.state.photoLanguage[5])}/>
+                                        <img className='imgDB' src={ require('../../img/languages/' + this.state.photoLanguage[5])}/>
                                     </Col>
                                 </Row>
                             </Col>
@@ -113,6 +120,7 @@ export default class home extends Component {
                     </Card>
                     <Card style={{padding:'0px 50px', backgroundColor: 'rgba(10, 255, 50, 255)', width: 'auto', height: '600px'}}>
                         <h1>PHOTO</h1>
+                        <Slider style={{ width: '100%', height: '100%'}} slides={images}/>
                     </Card>
                 </Container>
             </div>
